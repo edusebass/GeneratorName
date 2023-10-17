f2cdac916941ef5ba1e8d1972c47240a76cf2977
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-10-17 16:13:36 -0500

    .

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static("public"));
 // app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("/views/index.ejs");
 });
 
 // app.post("/submit", (req, res) => {
