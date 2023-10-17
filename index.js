57eefa23589fdf06a48e5e5ac4a51fe511d59884
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-10-17 16:05:07 -0500

    update prject

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static("public"));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
+// app.get("/", (req, res) => {
+//   res.render("index.ejs");
+// });
 
-app.post("/submit", (req, res) => {
-  const randomAdj = adj[Math.floor(Math.random() * adj.length)];
-  const randomNoun = noun[Math.floor(Math.random() * noun.length)];
-  res.render("index.ejs", {
-    adjective: randomAdj,
-    noun: randomNoun,
-  });
-});
+// app.post("/submit", (req, res) => {
+//   const randomAdj = adj[Math.floor(Math.random() * adj.length)];
+//   const randomNoun = noun[Math.floor(Math.random() * noun.length)];
+//   res.render("index.ejs", {
+//     adjective: randomAdj,
+//     noun: randomNoun,
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
