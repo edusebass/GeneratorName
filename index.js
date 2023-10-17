bbabd4bc0c1483ab1544918b33c48def82e088e7
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-10-17 16:17:43 -0500

    .

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 // app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/", (req, res) => {
-  res.render("index.ejs");
-});
+// app.get("/", (req, res) => {
+//   res.render("index.ejs");
+// });
 
 // app.post("/submit", (req, res) => {
 //   const randomAdj = adj[Math.floor(Math.random() * adj.length)];
